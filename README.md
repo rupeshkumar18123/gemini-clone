278f10d6fe3504d964f54a9ab6cd767474c8aa7c
Rupesh kumar <rupesh18123143@gmail.com>
2024-09-07 01:29:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#backend gemini clone
+npm init
+npm install
+main---> index.js
+
+npm install @google/generative-ai
+npm i express
+npm install cors
+npm  i body-parser
---
+++ backend_gemini_clone/index.js
@@ -0,0 +1,57 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable.
+const genAI = new GoogleGenerativeAI("AIzaSyB76L5mvSDPmG6W5pAFC5s5B-b49oVpucY");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/test",(req,res)=>{
+    res.send("website is working");
+});
+
+//generate promt answer using googel api
+let answer = '';
+
+app.post("/promt",async (req,resp)=>{
+   const {userpromt} = req.body;
+   answer = await run(userpromt);
+   resp.status(200).send({answer});
+   console.log(answer);
+});
+
+//configuring promt answer
+const generationConfig = {
+  stopSequences: ["red"],
+  maxOutputTokens: 1000,
+  temperature: 0.9,
+  topP: 0.1,
+  topK: 16,
+};
+
+async function run(userPromt) {
+    //choosing a model
+    const model = genAI.getGenerativeModel({model : "gemini-1.5-flash" , generationConfig});
+    
+    //define promt or modifying promt
+    const  prompt = userPromt + " in less than 300 words only";
+
+    const result = await model.generateContent(prompt);
+
+    const response = result.response;
+    const text  = response.text();
+
+    return text;
+}
+
+
+app.listen(3000,()=>{
+    console.log("everthing is fine...!!!");
+    console.log("server is running on port number :3000 \n [http://localhost:3000]");
+})
